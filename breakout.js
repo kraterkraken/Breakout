34fdd2b1f391e3fade83ae16027d3dc4aeb1bc08
Sales IT <SalesIT@autosoft.local>
2019-02-26 13:19:48 -0500

    consolidated some code into the GameEntity

--- breakout.js
+++ breakout.js
@@ -7,8 +7,7 @@ const CONFIG =
     brickColor : "red",
     ballColor : "lightblue",
     paddleColor : "yellow",
-//test
-// this is even nicer
+
     brickRows : 8,
     brickColumns : 7,
     brickSpacing : 5,
@@ -37,6 +36,8 @@ function normalizeAngle(radians)
     return newAngle;
 }
 
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
 class EZArt
 {
     static drawBox(ctx, color, x, y, width, height)
@@ -57,23 +58,48 @@ class EZArt
     }
 }
 
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
 class BreakoutGame
 {
+    // ---------------------------------------------------------------------
     constructor(canvasId)
     {
         // I'm not fond of defining instance variables this way, but this is how you do it (FOR NOW!) :-(
         this.canvas = document.getElementById(canvasId);
         this.ctx = this.canvas.getContext("2d");
-        this.entities = [];
         this.score = 0;
         this.livesLeft = CONFIG.startLives;
         this.running = false;
 
-        this.ball = null;
-        this.paddle = null;
+        // set up all the entities that the game needs to keep track of
+        this.ball = new Ball(CONFIG.ballColor, this.canvas.width/2, CONFIG.paddleY-CONFIG.ballRadius-1, CONFIG.ballRadius);
+        this.paddle = new Paddle(CONFIG.paddleColor, (this.canvas.width - CONFIG.paddleWidth)/2, CONFIG.paddleY, CONFIG.paddleWidth, CONFIG.paddleHeight);
         this.bricks = [];
+        this.entities = [];
+
+        this.entities.push(this.ball);
+        this.entities.push(this.paddle);
+
+        for (let i=0, y=0; i<CONFIG.brickRows; i++)
+        {
+            y += CONFIG.brickSpacing;
+            for (let j=0, x=0; j<CONFIG.brickColumns; j++)
+            {
+                x += CONFIG.brickSpacing;
+                let newBrick = new Brick(CONFIG.brickColor, x, y, CONFIG.brickWidth, CONFIG.brickHeight, CONFIG.brickValue);
+                this.entities.push(newBrick);
+                this.bricks.push(newBrick);
+                x += CONFIG.brickWidth;
+            }
+            y += CONFIG.brickHeight;
+        }
+
+        // one easy line to draw everything
+        this.draw();
     }
 
+    // ---------------------------------------------------------------------
     handleWallCollisions()
     {
         // check for wall collisions
@@ -103,6 +129,7 @@ class BreakoutGame
         }
     }
 
+    // ---------------------------------------------------------------------
     handleEntityCollisions()
     {
         if (this.ball.checkCollisionWith(this.paddle))
@@ -128,6 +155,7 @@ class BreakoutGame
         }
     }
 
+    // ---------------------------------------------------------------------
     draw()
     {
         for (let i=0; i<this.entities.length; i++)
@@ -136,15 +164,45 @@ class BreakoutGame
         }
     }
 
+    // ---------------------------------------------------------------------
     redraw()
     {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.draw(this.ctx);
     }
+
+    // ---------------------------------------------------------------------
+    moveBall()
+    {
+        // make the ball move
+        let s = (this.ball.speed/CONFIG.frameRate);
+        let xIncrement = s * Math.cos(this.ball.direction);
+        let yIncrement = s * Math.sin(this.ball.direction);
+        this.ball.x += xIncrement;
+        this.ball.y += yIncrement;
+    }
+
+    // ---------------------------------------------------------------------
+    checkLives()
+    {
+        if (this.ball.y + this.ball.radius >= this.canvas.height)
+        {
+            this.livesLeft -=1;
+            console.log("you lost a life!");
+            if (this.livesLeft == 0)
+            {
+                console.log("game over, man!  game over!");
+            }
+        }
+
+    }
 }
 
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
 class GameEntity
 {
+    // ---------------------------------------------------------------------
     constructor(color, x, y, speed=0, direction=0)
     {
         this.color = color;
@@ -160,14 +218,17 @@ class GameEntity
     get top() {return null;}
     get bottom() {return null;}
 
+    // ---------------------------------------------------------------------
     draw(ctx) { /* subclasses should override */ }
 
+    // ---------------------------------------------------------------------
     setPosition(x, y) // convenience method
     {
         this.x = x;
         this.y = y;
     }
 
+    // ---------------------------------------------------------------------
     checkCollisionWith(aGameEntity)
     {
         return false;
@@ -175,8 +236,11 @@ class GameEntity
 
 }
 
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
 class Ball extends GameEntity
 {
+    // ---------------------------------------------------------------------
     constructor(color, x, y, radius, speed=0, direction=0)
     {
         super(color, x, y, speed, direction);
@@ -188,11 +252,13 @@ class Ball extends GameEntity
     get top() {return this.y - this.radius;}
     get bottom() {return this.y + this.radius;}
 
+    // ---------------------------------------------------------------------
     draw(ctx)
     {
         EZArt.drawCircle(ctx, this.color, this.x, this.y, this.radius);
     }
 
+    // ---------------------------------------------------------------------
     checkCollisionWith(that)
     {
 
@@ -203,212 +269,169 @@ class Ball extends GameEntity
         }
 
         // two things have collided if there are no gaps between them, so look for gaps
-        if (this.right < that.left
-            || this.bottom < that.top
-            || that.right < this.left
-            || that.bottom < this.top)
+        if (this.right < that.left || this.bottom < that.top || that.right < this.left || that.bottom < this.top)
+        {
+            // we found gaps
+            return false;
+        }
+        else // we collided, so figure out where we collided and respond appropriately
+        {
+
+            // Observations:
+            // As this approaches that from the left, i and j (below) are positive.
+            // When they collide, i becomes negative.  j remains positive until this has completely
+            // passed through to the other side, at which point both i and j will be negative.
+            // Therefore, when overlapped i*j is negative. If i*j is positive, then they are not overlapped.
+            // Also whichever is smaller -- abs(i) or abs(j) -- indicates which side is likely the collision point.
+            // If abs(i) is smaller, then we collided from the left.  Otherwise, we collided from the right.
+            // (assumes that is not significantly smaller than this)
+
+            let i = this.right - that.left;
+            let j = that.right - this.left;
+            let m = this.bottom - that.top;
+            let n = that.bottom - this.top;
+
+            if (that.left <= this.x && this.x <= that.right) // if we hit the top or bottom
             {
-                // we found gaps
-                return false;
-            }
-            else // we collided, so figure out where we collided and respond appropriately
-            {
+                // rebound
+                this.direction = -this.direction;
 
-                // Observations:
-                // As this approaches that from the left, i and j (below) are positive.
-                // When they collide, i becomes negative.  j remains positive until this has completely
-                // passed through to the other side, at which point both i and j will be negative.
-                // Therefore, when overlapped i*j is negative. If i*j is positive, then they are not overlapped.
-                // Also whichever is smaller -- abs(i) or abs(j) -- indicates which side is likely the collision point.
-                // If abs(i) is smaller, then we collided from the left.  Otherwise, we collided from the right.
-                // (assumes that is not significantly smaller than this)
-
-                let i = this.right - that.left;
-                let j = that.right - this.left;
-                let m = this.bottom - that.top;
-                let n = that.bottom - this.top;
-
-                if (that.left <= this.x && this.x <= that.right) // if we hit the top or bottom
+                // adjust ball position a tad
+                if (Math.abs(m) < Math.abs(n))
                 {
-                    // rebound
-                    this.direction = -this.direction;
-
-                    // adjust ball position a tad
-                    if (Math.abs(m) < Math.abs(n))
-                    {
-                        // we hit from the top
-                        this.y = that.top - this.radius;
-                    }
-                    else
-                    {
-                        // we hit from the bottom
-                        this.y = that.bottom + this.radius;
-                    }
-                }
-                else if (that.top <= this.y && this.y <= that.bottom) // else if we hit the left or right
-                {
-                    // rebound
-                    this.direction = Math.PI - this.direction;
-
-                    //adjust ball position a tad
-                    if (Math.abs(i) < Math.abs(j))
-                    {
-                        // we hit from the left
-                        this.x = that.left - this.radius;
-                    }
-                    else
-                    {
-                        // we hit from the right
-                        this.x = that.right + this.radius;
-                    }
+                    // we hit from the top
+                    this.y = that.top - this.radius;
                 }
                 else
                 {
-                    // This is the edge case where we are overlapped corner to corner, but the ball's center
-                    // is not yet inside the vertical or horizontal bounds of aGameEntity.
-                    // Treat as a non-collision.
-                    return false;
+                    // we hit from the bottom
+                    this.y = that.bottom + this.radius;
                 }
-
-                return true;
-
             }
-        }
-
-    }
-
-    class Brick extends GameEntity
-    {
-        constructor(color, x, y, width, height, value=0, speed=0, direction=0)
-        {
-            super(color, x, y, speed, direction);
-            this.width = width;
-            this.height = height;
-            this.value = value;
-        }
-
-        get left() {return this.x;}
-        get right() {return this.x + this.width;}
-        get top() {return this.y;}
-        get bottom() {return this.y + this.height;}
-
-        draw(ctx)
-        {
-            if (this.exists)
+            else if (that.top <= this.y && this.y <= that.bottom) // else if we hit the left or right
             {
-                EZArt.drawBox(ctx, this.color, this.x, this.y, this.width, this.height);
+                // rebound
+                this.direction = Math.PI - this.direction;
+
+                //adjust ball position a tad
+                if (Math.abs(i) < Math.abs(j))
+                {
+                    // we hit from the left
+                    this.x = that.left - this.radius;
+                }
+                else
+                {
+                    // we hit from the right
+                    this.x = that.right + this.radius;
+                }
             }
-        }
-    }
-
-    class Paddle extends Brick
-    {
-    }
-
-    ///////////////////////////////////////////////////////////////////
-    ////////////////HERE IS WHERE THE MAGIC HAPPENS////////////////////
-    ///////////////////////////////////////////////////////////////////
-
-    var game = new BreakoutGame("breakoutCanvas");
-
-    function setup()
-    {
-        // set up all the entities that the game needs to keep track of
-        game.ball = new Ball(CONFIG.ballColor, game.canvas.width/2, CONFIG.paddleY-CONFIG.ballRadius-1, CONFIG.ballRadius);
-        game.paddle = new Paddle(CONFIG.paddleColor, (game.canvas.width - CONFIG.paddleWidth)/2, CONFIG.paddleY, CONFIG.paddleWidth, CONFIG.paddleHeight);
-
-        game.entities.push(game.ball);
-        game.entities.push(game.paddle);
-
-        for (let i=0, y=0; i<CONFIG.brickRows; i++)
-        {
-            y += CONFIG.brickSpacing;
-            for (let j=0, x=0; j<CONFIG.brickColumns; j++)
+            else
             {
-                x += CONFIG.brickSpacing;
-                let newBrick = new Brick(CONFIG.brickColor, x, y, CONFIG.brickWidth, CONFIG.brickHeight, CONFIG.brickValue);
-                game.entities.push(newBrick);
-                game.bricks.push(newBrick);
-                x += CONFIG.brickWidth;
+                // This is the edge case where we are overlapped corner to corner, but the ball's center
+                // is not yet inside the vertical or horizontal bounds of aGameEntity.
+                // Treat as a non-collision.
+                return false;
             }
-            y += CONFIG.brickHeight;
-        }
 
-        // one easy line to draw everything
-        game.draw();
+            return true;
+
+        }
     }
 
-    function mouseMoveHandler(e)
+}
+
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+class Brick extends GameEntity
+{
+    // ---------------------------------------------------------------------
+    constructor(color, x, y, width, height, value=0, speed=0, direction=0)
     {
-        // the middle of the paddle follows the mouse pointer
-        game.paddle.x = e.clientX - game.paddle.width/2;
-        //for some reason, the above line puts the paddle about 10px too far right, so adjust
-        game.paddle.x -= 10;
-
-        // don't go past the walls
-        if (game.paddle.left < 0)
-        {
-            game.paddle.x = 0;
-        }
-        if (game.paddle.right > game.canvas.width)
-        {
-            game.paddle.x = game.canvas.width - game.paddle.width;
-        }
-
-        // if the game hasn't begun, the ball should follow the paddle
-        if (!game.running)
-        {
-            game.ball.x = e.clientX;
-            //for some reason, the above line puts the ball about 10px too far right, so adjust
-            game.ball.x -= 10;
-        }
+        super(color, x, y, speed, direction);
+        this.width = width;
+        this.height = height;
+        this.value = value;
     }
 
-    function mouseClickHandler(e)
+    get left() {return this.x;}
+    get right() {return this.x + this.width;}
+    get top() {return this.y;}
+    get bottom() {return this.y + this.height;}
+
+    // ---------------------------------------------------------------------
+    draw(ctx)
     {
-        // the game starts when we click the mouse button
-        game.running = true;
-
-        // launch the ball
-        game.ball.speed = CONFIG.initialBallSpeed;
-        game.ball.direction = CONFIG.initialBallDirection;
-
-        // once we launch, we will never do it again, so remove the listener
-        document.removeEventListener("click", mouseClickHandler);
-
-    }
-
-    function playGame()
-    {
-
-
-        // check if the ball hit anything this frame
-        game.handleWallCollisions();
-        game.handleEntityCollisions();
-
-        // make the ball move
-        let s = (game.ball.speed/CONFIG.frameRate);
-        let xIncrement = s * Math.cos(game.ball.direction);
-        let yIncrement = s * Math.sin(game.ball.direction);
-        game.ball.x += xIncrement;
-        game.ball.y += yIncrement;
-
-        // see if we lost
-        if (game.ball.y + game.ball.radius >= game.canvas.height)
+        if (this.exists)
         {
-            game.livesLeft -=1;
-            console.log("you lost a life!");
-            if (game.livesLeft == 0)
-            {
-                console.log("game over, man!  game over!");
-            }
+            EZArt.drawBox(ctx, this.color, this.x, this.y, this.width, this.height);
         }
+    }
+}
 
-        // redraw everything
-        game.redraw();
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+class Paddle extends Brick
+{
+}
+
+// -------------------------------------------------------------------------
+// -------------------------- EVENT HANDLERS -------------------------------
+// -------------------------------------------------------------------------
+function mouseMoveHandler(e)
+{
+    // the middle of the paddle follows the mouse pointer
+    game.paddle.x = e.clientX - game.paddle.width/2;
+    //for some reason, the above line puts the paddle about 10px too far right, so adjust
+    game.paddle.x -= 10;
+
+    // don't go past the walls
+    if (game.paddle.left < 0)
+    {
+        game.paddle.x = 0;
+    }
+    if (game.paddle.right > game.canvas.width)
+    {
+        game.paddle.x = game.canvas.width - game.paddle.width;
     }
 
-    setup();
-    document.addEventListener("click", mouseClickHandler);
-    document.addEventListener("mousemove", mouseMoveHandler);
-    setInterval(playGame, 1000/CONFIG.frameRate);
+    // if the game hasn't begun, the ball should follow the paddle
+    if (!game.running)
+    {
+        game.ball.x = e.clientX;
+        //for some reason, the above line puts the ball about 10px too far right, so adjust
+        game.ball.x -= 10;
+    }
+}
+
+function mouseClickHandler(e)
+{
+    // the game starts when we click the mouse button
+    game.running = true;
+
+    // launch the ball
+    game.ball.speed = CONFIG.initialBallSpeed;
+    game.ball.direction = CONFIG.initialBallDirection;
+
+    // once we launch, we will never do it again, so remove the listener
+    document.removeEventListener("click", mouseClickHandler);
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////HERE IS WHERE THE MAGIC HAPPENS//////////////////
+////////////////////////////////////////////////////////////////////////////////
+var game = new BreakoutGame("breakoutCanvas");
+function playGame()
+{
+    // Check if the ball hit anything this frame, then make it move.
+    // After that, see if any lives were lost.  Finally, draw this frame.
+    game.handleWallCollisions();
+    game.handleEntityCollisions(); // ISSUE: is there a problem if the ball hits the wall and the paddle at the same time?
+    game.moveBall();
+    game.checkLives();
+    game.redraw();
+}
+
+document.addEventListener("click", mouseClickHandler);
+document.addEventListener("mousemove", mouseMoveHandler);
+setInterval(playGame, 1000/CONFIG.frameRate);
